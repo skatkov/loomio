d58bcd0d64f34bb2387295969bea0bdf2de93743
James Kiesel <james.kiesel@gmail.com>
2015-11-24 11:24:16 +0800

    Simpler discussion privacy for logged out users

--- app/extras/queries/visible_discussions.rb
+++ app/extras/queries/visible_discussions.rb
@@ -106,18 +106,21 @@ class Queries::VisibleDiscussions < Delegator
 
   def self.apply_privacy_sql(user: nil, group_ids: [], relation: nil)
     user ||= LoggedOutUser.new
-    group_ids = group_ids.presence || Group.visible_to_public.pluck(:id) unless user.is_logged_in?
 
-    # select where
-    # the discussion is public
-    # or they are a member of the group
-    # or user belongs to parent group and permission is inherited
-    relation.where('discussions.group_id in (:group_ids) AND
-                   ((discussions.private = false) OR
-                    (discussions.group_id IN (:user_group_ids)) OR
-                    (groups.parent_members_can_see_discussions = TRUE AND groups.parent_id IN (:user_group_ids)))',
-                   group_ids: group_ids,
-                   user_group_ids: user.group_ids)
+    if user.is_logged_in?
+      # select where
+      # the discussion is public
+      # or they are a member of the group
+      # or user belongs to parent group and permission is inherited
+      relation.where('discussions.group_id in (:group_ids) AND
+                      ((discussions.private = false) OR
+                       (discussions.group_id IN (:user_group_ids)) OR
+                       (groups.parent_members_can_see_discussions = TRUE AND groups.parent_id IN (:user_group_ids)))',
+                     group_ids: group_ids,
+                     user_group_ids: user.group_ids)
+    else
+      relation.where("groups.is_visible_to_public = 't'")
+    end
   end
 
 end
